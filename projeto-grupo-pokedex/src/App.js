b8e720b1546ccb0092fd353527fff272f455cf79
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-08-25 22:11:58 -0300

    lado esquerdo da pokedex, ira aparecer em todas as Pags

--- projeto-grupo-pokedex/src/App.js
+++ projeto-grupo-pokedex/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { Router } from './routes/Router';
+import { Router } from "./routes/Router";
+import styled from "styled-components";
+import { ButtonGlass, ButtonGlassGreen, ButtonGlassRed, ButtonGlassYellow, Curve1Left, Curve2Left, CurveLeft1, CurveLeft2, Junction, Junction1, Junction2, Left, PokedexAll, Rigth } from "./AppStyled";
 
 function App() {
   return (
-    <Router/>
+    <PokedexAll>
+      <Left>
+          <CurveLeft1/>
+          <CurveLeft2/>
+        <Curve1Left >
+          <ButtonGlass/>
+          <ButtonGlassRed/>
+          <ButtonGlassYellow/>
+          <ButtonGlassGreen/>
+        </Curve1Left>
+        <Curve2Left>
+          <Junction>
+            <Junction1/>
+            <Junction2/>
+          </Junction>
+        </Curve2Left>
+      </Left>
+      <Rigth></Rigth>
+      <Router />
+    </PokedexAll>
   );
 }
 
---
+++ projeto-grupo-pokedex/src/AppStyled.js
@@ -0,0 +1,227 @@
+import styled from 'styled-components'
+
+export const PokedexAll = styled.div`
+    width: 750px;
+    margin: 50px auto 0 auto;
+`
+
+export const Left = styled.div`
+    width: 400px;
+    height: 500px;
+    float: left;
+    position: relative;
+    z-index: 1;
+`
+export const Rigth = styled.div`
+
+    width: 350px;
+    height: 500px;
+    float: left;
+    position: relative;
+`
+
+
+export const Curve1Left = styled.div`
+
+    width: 201px;
+    height: 85px;
+    background-color: #8b0000;
+    float: left;
+    padding: 15px 0 0 15px;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    box-shadow: -10px 9px #5e0000;
+    -webkit-box-shadow: -10px 9px #5e0000;
+    -moz-box-shadow: -10px 9px #5e0000;
+    -o-box-shadow: -10px 9px #5e0000;
+
+    border-bottom-right-radius: 85px 60px;
+    -webkit-border-bottom-right-radius: 85px 60px;
+    -moz-border-bottom-right-radius: 85px 60px;
+    -o-border-bottom-right-radius: 85px 60px;
+
+    border-top-left-radius: 30px;
+    -webkit-border-top-left-radius: 30px;
+    -moz-border-top-left-radius: 30px;
+    -o-border-top-left-radius: 30px;
+`;
+
+export const CurveLeft1 = styled.div`
+
+
+    width: 400px;
+    height: 80px;
+    background-color: #8b0000;
+
+    border-top-left-radius: 30px;
+    -webkit-border-top-left-radius: 30px;
+    -moz-border-top-left-radius: 30px;
+    -o-border-top-left-radius: 30px;
+`
+
+export const CurveLeft2 = styled.div`
+
+        width: 400px;
+		height: 420px;
+		background-color: #c00d0d;
+
+		box-shadow: -10px 9px #5e0000;
+		-webkit-box-shadow: -10px 9px #5e0000;
+		-moz-box-shadow: -10px 9px #5e0000;
+		-o-box-shadow: -10px 9px #5e0000;
+
+		border-bottom-left-radius: 30px;
+		-webkit-border-bottom-left-radius: 30px;
+		-moz-border-bottom-left-radius: 30px;
+		-o-border-bottom-left-radius: 30px;
+`
+
+export const ButtonGlass = styled.button`
+
+    width: 60px;
+    height: 60px;
+    margin-right: 15px;
+    border: 5px solid #fff;
+    float: left;
+
+    box-shadow: 0 0 10px #490000;
+    -webkit-box-shadow: 0 0 10px #490000;
+    -moz-box-shadow: 0 0 10px #490000;
+    -o-box-shadow: 0 0 10px #490000;
+    
+    background: radial-gradient(#05fbfb, #29abe3);
+    background: -webkit-radial-gradient(#05fbfb, #29abe3);
+    background: -moz-radial-gradient(#05fbfb, #29abe3);
+    background: -o-radial-gradient(#05fbfb, #29abe3);
+    
+    border-radius: 35px;
+    -webkit-border-radius: 35px;
+    -moz-border-radius: 35px;
+       -o-border-radius: 35px;
+
+`
+
+export const ButtonGlassRed = styled.div`
+
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    float: left;
+
+    box-shadow: 0 0 10px #490000;
+    -webkit-box-shadow: 0 0 10px #490000;
+    -moz-box-shadow: 0 0 10px #490000;
+    -o-box-shadow: 0 0 10px #490000;
+
+    border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    -o-border-radius: 20px;
+    
+    background: radial-gradient(#fb7b7b, #fb0505);
+    background: -webkit-radial-gradient(#fb7b7b, #fb0505);
+    background: -moz-radial-gradient(#fb7b7b, #fb0505);
+    background: -o-radial-gradient(#fb7b7b, #fb0505);
+
+`
+
+export const ButtonGlassYellow = styled.div`
+
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    float: left;
+
+    box-shadow: 0 0 10px #490000;
+    -webkit-box-shadow: 0 0 10px #490000;
+    -moz-box-shadow: 0 0 10px #490000;
+    -o-box-shadow: 0 0 10px #490000;
+
+    border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    -o-border-radius: 20px;
+    
+    background: radial-gradient(#fbfb9b, #fbfb05);
+    background: -webkit-radial-gradient(#fbfb9b, #fbfb05);
+    background: -moz-radial-gradient(#fbfb9b, #fbfb05);
+    background: -o-radial-gradient(#fbfb9b, #fbfb05);
+`
+
+export const ButtonGlassGreen = styled.div`
+
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    float: left;
+
+    box-shadow: 0 0 10px #490000;
+    -webkit-box-shadow: 0 0 10px #490000;
+    -moz-box-shadow: 0 0 10px #490000;
+    -o-box-shadow: 0 0 10px #490000;
+
+    border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    -o-border-radius: 20px;
+    
+    background: radial-gradient(#b0fb7b, #50fb05);
+    background: -webkit-radial-gradient(#b0fb7b, #50fb05);
+    background: -moz-radial-gradient(#b0fb7b, #50fb05);
+    background: -o-radial-gradient(#b0fb7b, #50fb05);
+
+`
+
+export const Curve2Left = styled.div`
+		width: 216px;
+		height: 451px;
+		background-color: #c00d0d;
+		float: right;
+
+		position: absolute;
+		bottom: 0;
+		right: 0;
+
+		border-top-left-radius:85px 60px;
+		-webkit-border-top-left-radius: 85px 60px;
+		-moz-border-top-left-radius: 85px 60px;
+		-o-border-top-left-radius: 85px 60px;
+`
+
+export const Junction = styled.div`
+
+    width: 60px;
+    height: 451px;
+    float: right;
+    
+    background: linear-gradient(left, #8b0000 0%, #c00d0d 50%, #8b0000 100%);	
+    background: -webkit-linear-gradient(left, #8b0000 0%, #c00d0d 50%, #8b0000 100%);
+    background: -moz-linear-gradient(left, #8b0000 0%, #c00d0d 50%, #8b0000 100%);
+    background: -o-linear-gradient(left, #8b0000 0%, #c00d0d 50%, #8b0000 100%);
+
+`
+
+export const Junction1 = styled.div`
+
+    height: 40px;
+    background-color: #5e0000;
+    opacity: 0.3;
+    margin-top: 50px;
+    margin-bottom: 262px;
+
+    border-top: 2px solid #330000;
+    border-bottom: 2px solid #330000;
+`
+
+export const Junction2 = styled.div`
+
+    height: 40px;
+    background-color: #5e0000;
+    opacity: 0.3;
+
+    border-top: 2px solid #330000;
+    border-bottom: 2px solid #330000;
+`
